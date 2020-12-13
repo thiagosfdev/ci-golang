e9b948686b82def52056290a15e4f3fd8e0c02ed
Thiago Figueiredo <dev01.thiagosf@gmail.com>
2020-12-13 03:49:21 -0300

    feat: add hello.go

--- hello.go
+++ hello.go
@@ -1,11 +1,11 @@
-package main
+package calcs
 
 import "fmt"
 
 func main() {
-	fmt.Println(Soma(10, 10))
+	fmt.Println(Mult(10, 10))
 }
 
-func Soma(a int, b int) int {
-	return a + b
+func Mult(a int, b int) int {
+	return a * b
 }
